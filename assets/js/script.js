9c5152dacfb4e48646290d6fe4192a67fed301cf
crawnicles <crawnicles@gmail.com>
2025-08-11 19:04:24 -0500

    updated version

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('theme-toggle').addEventListener('click', () => {
+    document.body.classList.toggle('light-mode');
+    document.body.classList.toggle('dark-mode');
+});
---
+++ data/writings/sample.md
@@ -0,0 +1,2 @@
+# Sample Writing
+This is an example writing entry. Replace with your markdown files.
